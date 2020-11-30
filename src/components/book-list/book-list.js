3ea0e15620b6ab82ba376a628ed3fb5585f8f898
sergei2010 <sergejmatvejcuk323@gmail.com>
2020-11-30 22:16:06 +0200

    change reducer / index.js

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
 import './book-list.css';
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1.receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+        console.log(data);
+
+        // 2. dispatch action to store
+        this.props.booksLoaded(data);
+    }
 
     render() {
         const { books } = this.props;
@@ -28,4 +41,12 @@ const mapStateToProps = ({ books }) => {
   return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+   booksLoaded
+};
+
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
